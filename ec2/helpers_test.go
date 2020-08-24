873d3acbb9566b9e7ab5f7e1232a0f85aaaafb69
sendqueery <47192407+sendqueery@users.noreply.github.com>
2020-08-24 12:07:43 -0400

    update EC2 methods to use paged functions + add tests

--- ec2/helpers_test.go
+++ ec2/helpers_test.go
@@ -3,6 +3,7 @@ package ec2
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
 
 	mocks "github.com/disneystreaming/ssm-helpers/testing"
@@ -12,15 +13,14 @@ func TestGetEC2InstanceTags(t *testing.T) {
 	assert := assert.New(t)
 	mockSvc := &mocks.MockEC2Client{}
 
-	tags, _ := GetEC2InstanceTags(mockSvc, "i-123", "i-456", "i-789")
+	t.Run("", func(t *testing.T) {
+		tags, err := GetEC2InstanceTags(mockSvc, aws.StringSlice([]string{"i-123", "i-456", "i-789"}))
 
-	t.Run("instance tag slice size", func(t *testing.T) {
+		assert.NoError(err)
 		assert.Lenf(tags, 3, "Incorrect number of tag slices returned, got %d, expected 3", len(tags))
-		assert.Truef(tags[0].InstanceID == "i-123" && tags[1].InstanceID == "i-456" && tags[2].InstanceID == "i-789", "Incorrect instance ID returned in tag slice\n%v", tags)
-	})
+		assert.Contains(tags, []string{"i-123", "i-456", "i-789"}, "Incorrect instance ID returned in tag slice\n%v", tags)
+		assert.Equal(tags["i-123"]["env"], "dev", "Incorrect tag values returned for instance i-123, got , expected env:dev and id_foo:321")
 
-	t.Run("instance tag data", func(t *testing.T) {
-		assert.Truef(tags[0].Tags["env"] == "dev" && tags[0].Tags["id_foo"] == "321", "Incorrect tag values returned for instance i-123, got %v, expected env:dev and id_foo:321", tags[0].Tags)
 	})
 }
 
@@ -28,7 +28,12 @@ func TestGetEC2InstanceInfo(t *testing.T) {
 	assert := assert.New(t)
 	mockSvc := &mocks.MockEC2Client{}
 
-	info, _ := getEC2InstanceInfo(mockSvc, "i-123", "i-456", "i-789")
+	t.Run("get instance info from IDs", func(t *testing.T) {
+		info, err := getEC2InstanceInfo(mockSvc,
+			aws.StringSlice([]string{"i-123", "i-456", "i-789"}))
 
-	assert.Lenf(info[0].Reservations[0].Instances, 3, "Incorrect number of instances returned, got %d, expected 3", len(info[0].Reservations[0].Instances))
+		assert.Lenf(info, 3, "Incorrect number of instances returned, got %d, expected 3", len(info))
+		assert.NoError(err)
+	})
+
 }
