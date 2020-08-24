ace21efac3a74fbfb0574098bd18e7946d3f939f
sendqueery <47192407+sendqueery@users.noreply.github.com>
2020-08-24 12:09:20 -0400

    update SSM function to use paginated functions, move validation to server side

--- ssm/instance/fetch.go
+++ ssm/instance/fetch.go
@@ -1,54 +1,32 @@
 package instance
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
 )
 
-// getSSMInstances takes an SSM session and an *ssm.DescribeInstanceInformationInput object to get information about a given SSM-managed EC2 instance.
-func getSSMInstances(session ssmiface.SSMAPI, input *ssm.DescribeInstanceInformationInput) (output *ssm.DescribeInstanceInformationOutput, err error) {
-	return session.DescribeInstanceInformation(input)
-}
+func GetSessionInstances(client ssmiface.SSMAPI, diiInput *ssm.DescribeInstanceInformationInput) (output []*ssm.InstanceInformation, err error) {
+	// Fetch all instances that match the provided filters
+	if err = client.DescribeInstanceInformationPages(
+		diiInput,
+		func(page *ssm.DescribeInstanceInformationOutput, lastPage bool) bool {
+			for _, instance := range page.InstanceInformationList {
+				output = append(output, instance)
+			}
 
-// GetAllSSMInstances takes an SSM session and an *ssm.DescribeInstanceInformationInput object, and calls getSSMInstances() to query the SSM API for
-// information about a given SSM-managed EC2 instance. It also filters out any instances returned that are unresponsive to ping or not running Linux.
-// Filtering can only be done based on either instance information OR instance tags for any given instance of a query. As such, filtering of instances
-// by things like bamazon and env tags must be done after retrieving the instance information.
-func GetAllSSMInstances(session ssmiface.SSMAPI, input *ssm.DescribeInstanceInformationInput, checkLatestAgent bool) (output []*ssm.InstanceInformation, err error) {
-	// First call to set up NextToken
-	results, err := getSSMInstances(session, input)
+			// Last page, break out
+			if page.NextToken == nil {
+				return false
+			}
 
-	var allResults []*ssm.InstanceInformation
-	for _, v := range results.InstanceInformationList {
-		// Remove any instances that are unresponsive to ping or not running Linux
-		if checkLatestAgent {
-			if *v.PingStatus == "Online" && *v.PlatformType == "Linux" && *v.IsLatestVersion {
-				allResults = append(allResults, v)
-			}
-		} else {
-			if *v.PingStatus == "Online" && *v.PlatformType == "Linux" {
-				allResults = append(allResults, v)
-			}
-		}
+			// If not, set the token in order to fetch the next page
+			diiInput.SetNextToken(*page.NextToken)
+			return true
+		}); err != nil {
+		return nil, fmt.Errorf("Could not retrieve SSM instance info\n%v", err)
 	}
-	// Repeat call until we've paginated through all results
-	for results.NextToken != nil {
-		if *results.NextToken != "" {
-			input.SetNextToken(*results.NextToken)
-		}
-		results, _ = getSSMInstances(session, input)
-		for _, v := range results.InstanceInformationList {
-			// Remove any instances are unresponsive to ping or not running Linux
-			if checkLatestAgent {
-				if *v.PingStatus == "Online" && *v.PlatformType == "Linux" && *v.IsLatestVersion {
-					allResults = append(allResults, v)
-				}
-			} else {
-				if *v.PingStatus == "Online" && *v.PlatformType == "Linux" {
-					allResults = append(allResults, v)
-				}
-			}
-		}
-	}
-	return allResults, err
+
+	return output, err
 }
