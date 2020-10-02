fd66c648cd3fedbd32d0374a23f1222eb7770c7a
Josh Schell <joshua.schell@disneystreaming.com>
2020-10-02 12:40:03 -0400

    Updating explainer

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -27,19 +28,25 @@ func newRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "ssm",
 		Short: "ssm is a user-friendly wrapper for the AWS CLI SSM commands",
-		Long: `A Fast and Flexible Static Site Generator built with
-					  love by spf13 and friends in Go.
-					  Complete documentation is available at http://hugo.spf13.com`,
+		Long: `ssm is a user-friendly wrapper for the AWS CLI SSM commands
+
+Complete documentation is available at https://github.com/disneystreaming/ssm-helpers
+
+`,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			if err := setLogLevel(cmd, log); err != nil {
-				log.Fatal(err)
-			}
+			setLogLevel(cmd, args)
 			logutil.SetLogSplitOutput(log)
 		},
 		Version: fmt.Sprintf("%s\ngit commit hash %s", version, commit),
 	}
 
+	cmdutil.AddProfileFlag(cmd)
+	cmdutil.AddRegionFlag(cmd)
+	cmdutil.AddInstanceFlag(cmd)
+	cmdutil.AddDryRunFlag(cmd)
 	cmdutil.AddVerboseFlag(cmd)
+	cmdutil.AddAllProfilesFlag(cmd)
+	cmdutil.AddFilterFlag(cmd)
 
 	cmdgroup := &builder.SubCommandGroup{
 		Commands: []*cobra.Command{
@@ -52,9 +59,14 @@ func newRootCmd() *cobra.Command {
 	return cmd
 }
 
+func setLogLevel(cmd *cobra.Command, args []string) {
+	log.Level = logutil.IntToLogLevel(cmdutil.GetFlagInt(cmd, "verbose"))
+}
+
 // Execute provides an entrypoint into the commands from main()
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
